6ef3395d03175bf2488b7464fd89ef345522fea1
jess-bennett <jess.bennett@posteo.net>
2020-09-30 14:17:06 +0000

    Added base.html

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'task_manager'
-app.config["MONGO_URI"] = 'mongodb+srv://jessBennett:M0ng0DB@myfirstcluster.6abok.mongodb.net/task_manager?retryWrites=true&w=majority'
+app.config["MONGO_URI"] = 'mongodb+srv://jessBennett:M0ng0DB@myfirstcluster.\
+                          6abok.mongodb.net/task_manager?retryWrites=true&w=\
+                          majority'
 
 mongo = PyMongo(app)
 
