327c832bbcc3dbd36d01f02a78982e058d4f2a9b
jess-bennett <jess.bennett@posteo.net>
2020-09-30 09:59:43 +0000

    Commented out code causing errors

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template
+""" , render_template, redirect, request, url_for """
 from flask_pymongo import PyMongo
-from bson.objectid import ObjectId
+""" from bson.objectid import ObjectId """
 
 app = Flask(__name__)
-
 app.config["MONGO_DBNAME"] = 'task_manager'
-app.config["MONGO_URI"] = 'mongodb+srv://jessBennett:M0ng0DB@myfirstcluster.6abok.mongodb.net/task_manager?retryWrites=true&w=majority'
+app.config["MONGO_URI"] = """mongodb+srv://jessBennett:M0ng0DB@myfirst
+cluster.6abok.mongodb.net/task_manager?retryWrites=true&w=majority"""
 
 mongo = PyMongo(app)
+
 
 @app.route('/')
 @app.route('/get_tasks')
