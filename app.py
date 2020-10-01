3d678209b50db08439c9424af64071cd3f7da679
jess-bennett <jess.bennett@posteo.net>
2020-10-01 08:40:15 +0000

    Following updated videos

--- app.py
+++ app.py
@@ -1,25 +1,18 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for
-from flask_pymongo import PyMongo
-from bson.objectid import ObjectId
-from os import path
-if path.exists("env.py"):
+from flask import Flask
+if os.path.exists("env.py"):
     import env
 
+
 app = Flask(__name__)
-app.config["MONGO_DBNAME"] = os.environ.get('MONGODB_NAME')
-app.config["MONGO_URI"] = os.environ.get('MONGODB_URL')
-
-mongo = PyMongo(app)
 
 
-@app.route('/')
-@app.route('/get_tasks')
-def get_tasks():
-    return render_template("tasks.html", tasks=mongo.db.tasks.find())
+@app.route("/")
+def hello():
+    return "Hello World ... again!"
 
 
-if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
-            debug=os.environ.get('DEBUG_STATUS'))
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
